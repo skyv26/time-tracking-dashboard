591cc367c674e9f9c009cf26b8cd27ed9629e9a6
Aakash Verma <dev.aakashv@gmail.com>
2024-06-07 19:00:35 +0530

    `update`: the main component with `Stat` component

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import './style.css';
 import UserStatSwitch from './components/UserStatSwitch/UserStatSwitch';
+import Stat from './components/Stat/Stat';
 
 const main = document.createElement('main') as HTMLElement;
-main.insertAdjacentElement('afterbegin', UserStatSwitch() as HTMLElement);
+const section = document.createElement('section') as HTMLElement;
+section.className = 'SectionContainer';
+section.insertAdjacentElement('beforeend', UserStatSwitch() as HTMLElement);
+section.insertAdjacentElement('beforeend', Stat({
+    bgImage: '',
+    className: 'Stat'
+}) as HTMLElement);
+
+main.insertAdjacentElement('beforeend', section);
 
 document.querySelector('#app')?.insertAdjacentElement('afterbegin', main);
