68e6f9ec60c87c119eb34ed7de52ebc50fc19982
Aakash Verma <dev.aakashv@gmail.com>
2024-06-09 00:48:21 +0530

    `add`: the projects colors into the configuration file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ["./src/**/*.{html,js,ts}",],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        primary: {
+          blue: 'hsl(246, 80%, 60%)',
+          lightred: 'hsl(15, 100%, 70%)',
+          softblue: 'hsl(195, 74%, 62%)',
+          lightred: 'hsl(348, 100%, 68%)',
+          limegreen: 'hsl(145, 58%, 55%)',
+          violet: 'hsl(264, 64%, 52%)',
+          softorange: 'hsl(43, 84%, 65%)'
+        }
+      },
+    },
+    colors: {
+      very_dark_blue: 'hsl(226, 43%, 10%)',
+      dark_blue: 'hsl(235, 46%, 20%)',
+      desaturated_blue: 'hsl(235, 45%, 61%)',
+      pale_blue: 'hsl(236, 100%, 87%)',
+    }
   },
   plugins: [],
 };
