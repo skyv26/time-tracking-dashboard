8e97d3f627190cdf78f4e71beaa18e377f936e64
Aakash Verma <dev.aakashv@gmail.com>
2024-06-08 23:33:20 +0530

    `update`: the main file by add the container div for the stat

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,15 @@ const main = document.createElement('main') as HTMLElement;
 const section = document.createElement('section') as HTMLElement;
 section.className = 'SectionContainer';
 section.insertAdjacentElement('beforeend', UserStatSwitch() as HTMLElement);
-section.insertAdjacentElement('beforeend', Stat({
+
+const StatContainer = document.createElement('div') as HTMLDivElement;
+
+StatContainer.insertAdjacentElement('beforeend', Stat({
     bgImage: '',
     className: 'Stat'
 }) as HTMLElement);
+
+section.insertAdjacentElement('beforeend', StatContainer);
 
 main.insertAdjacentElement('beforeend', section);
 
