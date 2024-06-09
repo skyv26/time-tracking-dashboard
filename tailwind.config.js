d4e9c49f2914bed551ef07929a938547d045b76b
Aakash Verma <dev.aakashv@gmail.com>
2024-06-10 01:05:57 +0530

    `fix`: the color name in the configuration for the background

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,12 @@ export default {
       backgroundColor: {
         primary: {
           blue: 'hsl(246, 80%, 60%)',
-          lightred: 'hsl(15, 100%, 70%)',
-          softblue: 'hsl(195, 74%, 62%)',
-          lightred: 'hsl(348, 100%, 68%)',
-          limegreen: 'hsl(145, 58%, 55%)',
-          violet: 'hsl(264, 64%, 52%)',
-          softorange: 'hsl(43, 84%, 65%)'
+          work: 'hsl(15, 100%, 70%)',
+          play: 'hsl(195, 74%, 62%)',
+          study: 'hsl(348, 100%, 68%)',
+          exercise: 'hsl(145, 58%, 55%)',
+          social: 'hsl(264, 64%, 52%)',
+          selfcare: 'hsl(43, 84%, 65%)'
         }
       },
     },
