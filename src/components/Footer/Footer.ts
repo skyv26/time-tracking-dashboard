e78ba9b1397935be5f3a36fe507247aa0c3988ac
Aakash Verma <dev.aakashv@gmail.com>
2024-06-17 00:32:44 +0530

    `add`: the footer with acknowledgment

---
+++ src/components/Footer/Footer.ts
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return `
+        <footer class="footer">
+            <p>Challenge by
+                <a class="hover:text-[#ff74ca]" href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+                Made with ❤️ by <a class="hover:text-[#ff74ca] hover:underline" href="https://linkedin.com/in/skyv2022" target="_blank">Aakash Verma</a>
+            </p>
+        </footer>
+    `
+};
+
+export default Footer;
